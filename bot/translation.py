b2e4a0a54374c3c87e9b2e99effd6255f3923e89
LuciferMornigstarDevil <91133525+LuciferMornigstarDevil@users.noreply.github.com>
2021-09-21 20:46:12 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -1,36 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @Lucifer_Devil_AD
+# (c) @AlbertEinsteinTG @ADMOVEIAD
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {} No Start !!</b>
+    START_TEXT = """<b>Hey {} How are You!!</b>
 <i>
 നീ ഏതാ മോനൂസെ എന്നെ
-😊𝗣𝗢𝗣𝗖𝗢𝗥𝗡😊 ഗ്രൂപ്പിൽ മാത്രമേ
+ 😊𝗣𝗢𝗣𝗖𝗢𝗥𝗡😊ഗ്രൂപ്പിൽ മാത്രമേ
 ഉപയോഗിക്കാൻ പറ്റൂ...
 
 ©️MᴀɪɴᴛᴀɪɴᴇD Bʏ : @Lucifer_Devil_AD
-
-
+<b>😎വേറെ പണിയൊന്നുമില്ലേ വെറുതെ Bot ഇനെ Start അടിച്ച് സമയം കളയാതെ <a href="https://telegra.ph/file/0be0f517b928559625bff.jpg">😎</a></b>"""    
 
 
    
     
     HELP_TEXT = """
 <i>
- നീ ഏതാ..... ഒന്ന് പോടെയ് അവൻ help ചോയ്ച്ച് വന്നിരിക്കുന്നു😤...I'm Different Bot U Know
+  നീ ഏതാ..... ഒന്ന് പോടെയ് അവൻ help ചോയ്ച്ച് വന്നിരിക്കുന്നു😤...I'm Different Bot U Know
 
 
 """
     
-    ABOUT_TEXT = """<b>➥ My Name</b> : <code> Auto Filter BotV2.3</code>
+    ABOUT_TEXT = """<b>➥ My Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ My Creator</b> : <b><i><a href="https://t.me/Lucifer_Devil_AD</a></i></b>
+<b>➥ My Creator</b> : <b><i><a href="https://t.me/Lucifer_Devil_AD">Rafeeq_Kunnimon</a></i></b>
 
 <b>➥ My Language</b> : <code>Python3</code>
 
-<b>➥ My Library</b> : <i><a href="https://telegra.ph/file/0be0f517b928559625bff.jpg </a></i>
+<b>➥ My Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://telegra.ph/file/0be0f517b928559625bff.jpg</a></i>
+<b>➥ Source Code</b> : <i><a href="">Click Me</a></i>
 """
